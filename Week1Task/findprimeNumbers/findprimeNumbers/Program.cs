8adf35e7b0498c3db78244f997be0a405bcfc77b
Dilara Yıldırım <dilara.yldrm00@outlook.com.tr>
2022-03-23 14:23:31 +0300

    Proje dosyası ekle.

---
+++ Week1Task/findprimeNumbers/findprimeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace findprimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int numbers = 1; numbers <=100; numbers++)
+            {
+                int control = 0;
+
+                for (int i = 2; i < numbers; i++)
+                {
+                    if (numbers % i ==0)
+                    {
+                        control = 1;
+                        break;
+                    }
+                }
+                if (control ==0)
+                {
+                    Console.WriteLine("{0}  The number you entered is prime.", numbers);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
