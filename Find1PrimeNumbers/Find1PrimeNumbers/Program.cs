018a68c930354bf9d10c56d2b415da1cc68c950e
Dilara Yıldırım <dilara.yldrm00@outlook.com.tr>
2022-04-09 13:58:25 +0300

    find1primenumbers

---
+++ Find1PrimeNumbers/Find1PrimeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Find1PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int Numbers = 0;
+            Console.WriteLine("Enter the numbers: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    Numbers++;
+                }
+            }
+            if (Numbers == 0)
+            {
+                Console.WriteLine("The number you entered is prime.");
+            }
+            else
+            {
+                Console.WriteLine("The number you entered is not prime.");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
